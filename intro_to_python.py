43845bfe3c4cc8291b3781909a5a27d5550948c0
Keshawn Smith <keshawnchristopher@gmail.com>
2021-08-31 23:28:55 -0400

    Second Commit

---
+++ intro_to_python.py
@@ -0,0 +1,40 @@
+meal_completed = True
+total = 100
+tip = total * 1/5
+total = total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
+
+first = 'Springer'
+second = 'Bregman'
+third = 'Altvue'
+
+print(first)
+
+first = third
+
+print(first)
+
+#sentence = 'the quick brown fox jump over the lazy dog'
+#print(sentence)
+
+# sentence = 'The quick brown fox jumped'
+# sentence -> variable
+# 'The quick brown fox jumped' -> string
+# = -> assignment
+
+sentence = 'the quick brown fox jumped over the lazy dog'
+sentence_two = sentence.upper()
+
+
+print(sentence)
+print(sentence_two)
+
+sentence = 'the quick brown fox jumped over the lazy dog'.capitalize()
+print(sentence)
+
+sentence = 'the quick brown fox jumped over the lazy dog'.title()
+print(sentence)
+
+sentence = 'THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG'.lower()
+print(sentence)
