32de3e444674ac4fccafdde9bc6525d484906a82
Keshawn Smith <keshawnchristopher@gmail.com>
2021-09-02 00:02:06 -0400

    Third Commit

--- intro_to_python.py
+++ intro_to_python.py
@@ -38,3 +38,41 @@ print(sentence)
 
 sentence = 'THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG'.lower()
 print(sentence)
+
+# The quick brown fox jumped
+# T => 0
+# h => 1
+# e => 2
+# ' ' => 3
+
+starter_sentence = 'The quick brown fox jumped'
+
+new_sentence = 'Thy' + starter_sentence[3:]
+
+print(new_sentence)
+
+# Heredoc
+
+content = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+""".strip()
+
+print(content)
+
+content = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+"""
+
+print(repr(content))
+
+long_string = '\nNullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.\n\nVestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.\n\nInteger posuere erat a ante venenatis dapibus posuere velit aliquet.\n'
+
+print(long_string)
